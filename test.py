287f7eba5c119ba7306cbb95914b89f32473f0e2
Robert Bulek <norbikxs@gmail.com>
2022-06-12 00:33:32 +0200

    New Version of test random schedule

--- test.py
+++ test.py
@@ -10,23 +10,38 @@ def randomdays(num_days, num_workers):
 	for i in range(num_days):
 		dzien = []
 		for pracownik in num_workers:
-			if pracownik != "TOTAL":
-				t_n = choice(wybor)
-				dzien.append(t_n)
-			else:
-				godziny = 0
-				for day in dzien:
-					if day == "11-19":
-						godziny += 8
-				dzien.append(godziny)
+			t_n = choice(wybor)
+			dzien.append(t_n)
+
 		pracuje.append(dzien)
 
 
 	return pracuje
 
+def godziny(lista, num_days, num_workers):
+	godzin = []
+	for i in range(num_workers):
+		hours = 0
+		for j in range(num_days):
+			if lista[j][i] == "11-19":
+				hours+=8
+		godzin.append(hours)
+
+	return godzin
+
+
+
 num_days = monthrange(2022,6)[1]
 days = pd.period_range(start='2022-06-01', end='2022-06-30', freq='D')
-lista = ["Ania", "Hania", "Ewelina","Arek","TOTAL"]
+lista = ["Ania", "Hania", "Ewelina","Arek"]
 b = randomdays(num_days, lista)
-df = pd.DataFrame(index=days, columns=lista, data = b)
+df = pd.DataFrame(index=[days], columns=lista, data = b)
+
+
+c = godziny(b,num_days,len(lista))
+df_new = pd.DataFrame([c], columns = lista, index=['Total'])
+df = pd.concat([df, df_new])
+
+
+
 df.to_excel("grafik.xlsx")
