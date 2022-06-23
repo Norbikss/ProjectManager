0cc3a1de44e205497907763b83bf18e628bd914f
Robert Bulek <norbikxs@gmail.com>
2022-06-23 18:17:31 +0200

    Added databases

--- PM/schedule/admin.py
+++ PM/schedule/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Profile, Employee,Work_day, Vacation, Free_days
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Employee)
+admin.site.register(Work_day)
+admin.site.register(Vacation)
+admin.site.register(Free_days)
--- PM/schedule/models.py
+++ PM/schedule/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Employee(models.Model):
+    employer = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    name = models.CharField(max_length=25)
+    surname = models.CharField(max_length=25)
+    position = models.CharField(max_length=50)
+    etat = (
+        ("F", 'Full-Time'),
+        ("H", 'Half-Time'),
+        ("Q", 'Quatter-Time')
+    )
+    work_time = models.CharField(max_length=1, choices=etat)
+
+    def __str__(self):
+    	return self.name
+
+
+class Work_day(models.Model):
+    employer = models.ForeignKey(User, on_delete=models.CASCADE)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, default=employer)
+    day = models.DateField()
+    hours = (
+        ("A", '9-17'),
+        ("B", '11-19'),
+        ("C", '13-21'),
+        ("G", '9-21')
+    )
+    time = models.CharField(max_length=1, choices=hours)
+    worked_hours = models.IntegerField()
+    def __str__(self):
+    	return f'{self.day} {self.employee}'
+
+class Vacation(models.Model):
+    employer = models.ForeignKey(User, on_delete=models.CASCADE)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    def __str__(self):
+    	return self.employee
+
+class Free_days(models.Model):
+    employer = models.ForeignKey(User, on_delete=models.CASCADE)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    date = models.DateField()
+    def __str__(self):
+    	return self.employee
